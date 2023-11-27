bc599b229114a220b99bf3dd0adfa96c6668cf3e
Antonio Carlos de Lima Junior <ac@marketmining.com.br>
2023-11-27 15:42:34 -0300

    move main.py to root

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ action_choices = ["action1", "action2"]
 
 
 def main() -> None:
+    from src import app
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--action", choices=action_choices, required=False, default="action1"
@@ -16,8 +18,7 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    logging.info(args.action)
-    logging.info(args.param1)
+    app.run(args.action, args.param1)
 
 
 if __name__ == "__main__":
---
+++ src/app.py
@@ -0,0 +1,6 @@
+import logging
+
+
+def run(action: str, param1: str) -> None:
+    logging.info(action)
+    logging.info(param1)
